b5179cd15baea0f359e368e955dd2d2839d6b3a1
Lilit-Hov <hovakimyanlilit.a@gmail.com>
2021-10-26 10:40:56 +0400

    Careers accordion

--- flask_app/static/scripts/script.js
+++ flask_app/static/scripts/script.js
@@ -1,3 +1,4 @@
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -5,5 +6,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+    });
+});
+
+// accordion
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+    const btn = question.querySelector(".question-btn");
+
+    btn.addEventListener("click", () => {
+        //console.log(question);
+        questions.forEach((item) => {
+            if (item !== question) {
+                item.classList.remove("show-text");
+                //console.log(item);
+            }
+        });
+
+        question.classList.toggle("show-text");
+
+        //console.log(question);
     });
 });
