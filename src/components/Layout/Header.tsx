de1e211c00f59128e28b6ebd612ba2724118f4f9
Ashot Arzumanyan <aarzumanyan@moneylion.com>
2022-10-04 13:51:24 +0400

    fix camelcase error

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styled from 'styled-components';
-import logo from '../../../public/images/Logo.svg';
+import logo from '../../../public/images/logo.svg';
 
 const Container = styled.nav`
   display: flex;
