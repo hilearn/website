26d07129c17392e97d5ddfe32225ddf7e8ae0764
ashot1arzumanyan <ashot1arzumanyan@gmail.com>
2022-10-05 15:52:33 +0400

    add Team block (#24)

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+import arrow from '../../../public/images/long_right.svg'
+import Typography from "./Typography";
+
+interface ComponentProps {
+  children: ReactNode;
+}
+
+const StyledButton = styled.button`
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 24px;
+  padding: 8px 24px;
+  border: 1px solid #000000;
+  display: flex;
+`;
+
+const StyledTypography = styled(Typography)`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 21px;
+  margin-right: 18px;
+`;
+
+const Button = ({ children }: ComponentProps) => {
+  return (
+    <StyledButton>
+      <StyledTypography>
+        {children}
+      </StyledTypography>
+      <Image src={arrow} alt="" />
+    </StyledButton>
+  );
+};
+
+export default Button;
