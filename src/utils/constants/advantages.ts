1b1c2f31052bd80f92b503634f3ca9bfb97f74b6
ashot1arzumanyan <ashot1arzumanyan@gmail.com>
2022-10-10 17:58:21 +0400

    update advantages list (#64)

--- src/utils/constants/advantages.ts
+++ src/utils/constants/advantages.ts
@@ -5,24 +5,24 @@ interface Advantage {
 
 const advantages: Advantage[] = [
   {
-    title: 'Cross-tier teams',
-    description: 'Products are developed with an active engagement of different roles. We provide synergy between our multi-tiered teams to help your business to achieve the goals on time, with scale and high quality.'
+    title: 'Ability to solve hard AI/ML Problems',
+    description: 'Our engineering and science expertise covers AI/ML, Data Science / Data Mining and Quantitative Analysis. With our team\'s (5 International Math and Physics Olympiad winners) expertise you will be able to apply state of the art AI/ML solutions to your problems.',
   },
   {
-    title: 'Customers first',
-    description: 'We are becoming not only your trusted partner but also a natural part of your business and staff. We use all the up-to-date technologies (tools and frameworks) to ensure smooth cooperation with all the teams of the customer, just becoming their continuation to achieve OKRs and get product enhancements on time.'
+    title: 'Engineering Excellence',
+    description: 'We apply Engineering Excellence programs learned at Silicon Valley companies and equip our engineers with all the new technology trends and frameworks.',
+  },
+  {
+    title: 'Cross-tier teams',
+    description: 'We develop products with an active engagement of different roles. We provide synergy between our multi-tiered teams to help your business to achieve the goals on time, with scale and high quality.'
+  },
+  {
+    title: 'Partners first',
+    description: 'We become not only your trusted partner but also a natural part of your business and staff. We use all the up-to-date technologies (tools and frameworks) to ensure smooth cooperation with all the teams of the partner and, thus, helping to achieve the OKRs and get product enhancements.',
   },
   {
     title: 'Industry expertise',
-    description: 'We train our teams with all the peculiarities of the industries we engage with our partners. We are quite profound with FinTech, HealhTech, eCommerce, GovTech and other sectors that are locomotives for the world’s digital development agenda.'
-  },
-  {
-    title: 'Engineering',
-    description: 'We apply Engineering Excellence programs and equip our engineers with all the new technology trends and frameworks.'
-  },
-  {
-    title: 'New technologies',
-    description: 'Our engineering expertise covers AI, ML, Blockchain and Data Research. With our teams’ expertise you will be the global leaders in your industry.'
+    description: 'We train our teams with all the peculiarities of the industries we engage with our partners. We are quite profound with FinTech, HealthTech, eCommerce, GovTech and other sectors.',
   }
 ];
 
