9991a1c8b0334dbf60d1349c690c79ca48a9b7da
ashot1arzumanyan <ashot1arzumanyan@gmail.com>
2022-10-26 16:04:08 +0400

    add image meta tag and cache control for statics (#79)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import Head from "next/head";
 
 import Home from "../src/components/Home/Home";
+import preview from "../public/images/preview.jpg";
+
+const title = 'HiLearn | AI/ML, Data Science and Quantitative Analysis. Web & Mobile Development';
 
 const HomePage = () => {
   return (
     <>
       <Head>
-        <title>HiLearn | AI/ML, Data Science and Quantitative Analysis. Web & Mobile Development</title>
+        <title>{title}</title>
+        <meta name="title" property="og:title" content={title} />
+        <meta name="image" property="og:image" content={preview.src} />
+        <meta name="twitter:image" content={preview.src} />
       </Head>
       <Home />
     </>
--- pages/team.tsx
+++ pages/team.tsx
@@ -1,12 +1,18 @@
 import Head from "next/head";
 
 import Team from "../src/components/Team/Team";
+import preview from "../public/images/preview.jpg";
+
+const title = 'HiLearn | Team';
 
 const TeamPage = () => {
   return (
     <>
       <Head>
-        <title>HiLearn | Team</title>
+        <title>{title}</title>
+        <meta name="title" property="og:title" content={title} />
+        <meta name="image" property="og:image" content={preview.src} />
+        <meta name="twitter:image" content={preview.src} />
       </Head>
       <Team />
     </>
